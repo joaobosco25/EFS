861410ff0a6fe776fa9b489bb5512ccf76301994
Joao Bosco <bosco.dev@hotmail.com>
2025-11-27 18:02:18 -0300

    modal

--- js/script.js
+++ js/script.js
@@ -277,3 +277,21 @@ document.addEventListener('DOMContentLoaded', function() {
   }
 });
 
+const modal = document.getElementById("meuModal");
+const fechar = document.querySelector(".fechar");
+
+document.querySelectorAll(".abrir-modal").forEach((botao) => {
+  botao.addEventListener("click", () => {
+    modal.style.display = "flex";
+  });
+});
+
+fechar.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+window.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
